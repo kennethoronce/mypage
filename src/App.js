0bc5175a7300d668c66215fddaa0f43616b46be4
K oronce <kenneth.oronce@ecashpay.com.ph>
2021-04-29 16:35:21 +0800

    Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import img from "./img/dade.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Dade ko!</h1>
+      <h1>Mahal na mahal kita!</h1>
+      <img src={img} alt="image" />
     </div>
   );
 }
